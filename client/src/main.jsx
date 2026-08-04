83e3f1ea7a1eaeda3a9346cf3a19a58cf67f0c73
ankitgithub2101 <ankitrokz9401@gmail.com>
2026-08-04 15:07:50 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import { Provider } from "react-redux";
 import store from "./redux/store.jsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
-console.log(import.meta.env.VITE_GOOGLE_CLIENT_ID);
+// console.log(import.meta.env.VITE_GOOGLE_CLIENT_ID);
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
