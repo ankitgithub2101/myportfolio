a8d93edf22e046f3f5c324635e9ab9c34c194b5e
ankitgithub2101 <ankitrokz9401@gmail.com>
2026-08-07 19:42:49 +0530

    updated

--- client/src/ProtectedRoute/About.jsx
+++ client/src/ProtectedRoute/About.jsx
@@ -1,20 +1,36 @@
 import React from "react";
+import { TypeAnimation } from "react-type-animation";
 import "../Components/Book/Book.css";
 
 function About() {
   return (
-    <section className="face face--about">
-      <p className="kicker">About</p>
-
+    <section className="face">
       <h1 className="face__title">Who I Am</h1>
 
       <p className="face__body">
-        I am a developer focused on creating modern, interactive web experiences
-        with React, JavaScript and creative design.
+        I'm a B.E. graduate from Mumbai University passionate about, <br />
+        <span className="typing-text">
+          <TypeAnimation
+            sequence={[
+              "Frontend Development",
+              1500,
+              "Full Stack Development",
+              1500,
+              "MERN Stack Development",
+              1500,
+            ]}
+            wrapper="span"
+            speed={30}
+            repeat={Infinity}
+          />
+        </span>
+        <br />
+        .I enjoy building modern, responsive web applications and continuously
+        learning new technologies.
       </p>
 
       <div className="face__footer">
-        <span>About Page</span>
+        <span>About Me</span>
         <span>01</span>
       </div>
     </section>
